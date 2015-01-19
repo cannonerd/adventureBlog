ee39471ab6201ff70bccb9faf0ad402911efc942
Susanna <susanna.huhtanen@basilicom.de>
2015-01-19 14:35:50 +0100

    rm unnecessary semicolon

--- spec/test.timestamps.js
+++ spec/test.timestamps.js
@@ -10,7 +10,7 @@ describe("timestamp handling", function(){
       var corrected = correctTimestamp(time, 0);
       if(time.getTime() !== corrected.getTime()){
         throw new Error ("Times don't match");
-      };
+      }
     });
 
     it("Should alter the timestamps by negative hour", function(){
@@ -19,18 +19,12 @@ describe("timestamp handling", function(){
       var corrected = correctTimestamp(time, -60);
       /*calculate minutes to milliseconds so that the DateObject time is reduced correctly*/
       chai.expect(corrected.getTime()).to.equal(time.getTime()-60*60*1000);
-
-
     });
 
     it("Should alter the timestamps by a positive hour", function(){
       var time = new Date("2014-11-21T14:05:12Z");
       var corrected = correctTimestamp(time, 60);
       chai.expect(corrected.getTime()).to.equal(time.getTime()+60*60*1000);
-
-
     });
-
   });
-
 });
