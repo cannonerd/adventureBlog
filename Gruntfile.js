f27ad9b55dcc312042895369b301179828a9c45c
Susanna <susanna.huhtanen@basilicom.de>
2015-01-19 12:26:52 +0100

    add jshint to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,37 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    mocha_phantomjs: {
-    options: {
-      'reporter': 'spec',
-      'output': 'tests/results/result.xml',
-      urls: [
-          'http://localhost:8000/spec/runner.html',
-          'http://localhost:8000/src/clicker.html'
-        ]
-    },
-    all: ['spec/runner.html']
-    
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        mocha_phantomjs: {
+            options: {
+                'reporter': 'spec',
+                'output': 'tests/results/result.xml',
+                urls: [
+                    'http://localhost:8000/spec/runner.html',
+                    'http://localhost:8000/src/clicker.html'
+                ]
+            },
+            all: ['spec/runner.html']
 
-  },
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          base: '.'
+
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: '.'
+                }
+            }
         }
-      }
-}
-  });
+    });
 
 //make sure that all the necessary tasks are also defined in
 //the dependencies in package.json
-grunt.loadNpmTasks('grunt-contrib-connect');
-this.registerTask('test', ['connect', 'mocha_phantomjs']);
-grunt.loadNpmTasks('grunt-mocha-phantomjs');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    this.registerTask('test', ['connect', 'mocha_phantomjs']);
+    grunt.loadNpmTasks('grunt-mocha-phantomjs');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
 
 };
