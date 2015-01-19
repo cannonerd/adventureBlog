9b3372374b6e464f4c4a913425bd50d3a0a40fa0
Susanna <susanna.huhtanen@basilicom.de>
2015-01-19 12:35:04 +0100

    add jshint to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        jshint: { all: ['src/*.js'] },
         mocha_phantomjs: {
             options: {
                 'reporter': 'spec',
@@ -29,9 +30,10 @@ module.exports = function(grunt) {
 //make sure that all the necessary tasks are also defined in
 //the dependencies in package.json
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     this.registerTask('test', ['jshint','connect', 'mocha_phantomjs']);
     grunt.loadNpmTasks('grunt-mocha-phantomjs');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+
 
 
 };
