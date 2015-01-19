2dce1d4ef178019f717ad2e044a6e2f94f21c4ff
Susanna <susanna.huhtanen@basilicom.de>
2015-01-19 11:46:21 +0100

    tests fail, debugging

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,15 +6,29 @@ module.exports = function(grunt) {
     mocha_phantomjs: {
     options: {
       'reporter': 'xunit',
-      'output': 'tests/results/result.xml'
+      'output': 'tests/results/result.xml',
+      urls: [
+          'http://localhost:8000/spec/runner.html',
+          'http://localhost:8000/src/clicker.html'
+        ]
     },
-    all: ['spec/runner.html']
+    all: ['spec/runner.html'],
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.',
+        }
+      }
+    }
   }
   });
 
 //make sure that all the necessary tasks are also defined in
 //the dependencies in package.json
+grunt.loadNpmTasks('grunt-contrib-connect');
 grunt.loadNpmTasks('grunt-mocha-phantomjs');
+
 
 };
 
