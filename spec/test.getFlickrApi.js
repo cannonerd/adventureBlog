57d9fada89841b998131aa210a707696495ee2c7
Susanna <susanna.huhtanen@basilicom.de>
2015-01-19 15:53:31 +0100

    created a test for flickr api call

--- spec/test.getFlickrApi.js
+++ spec/test.getFlickrApi.js
@@ -2,7 +2,7 @@ describe("getting flickr api", function(){
     describe("flickr requests", function(){
         it("Should get photos from flickr based on set url ", function(){
             var url = "https://www.flickr.com/photos/ihmis-suski/sets/72157649813813025/";
-            chai.expect(url.getFlickrSet()).to.be.a("string");
+            chai.expect(getFlickrSet(url)).to.be.a("string");
         });
     })
 });
